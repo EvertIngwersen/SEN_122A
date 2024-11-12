b737309e05f56004cbed2e31fccfd8a6deb90939
Evert Ingwersen <everti2000@gmail.com>
2024-11-12 12:50:44 +0100

    added lists

--- Q2_2024_Statistical/Lab_sessions/Lab_session_00/lab_session_00_spyder.py
+++ Q2_2024_Statistical/Lab_sessions/Lab_session_00/lab_session_00_spyder.py
@@ -13,3 +13,28 @@ int_quotient = int(quotient)
 
 remainder = dividend % divisor 
 
+#defining a list
+
+# Lists
+
+# Defining a list
+this_is_an_empty_list = list()
+this_is_also_an_empty_list = []
+this_is_a_list = [4, 5, 'this is text', True]
+print(f'The original list is: {this_is_a_list}')
+
+# Accessing to elements
+result1 = this_is_a_list[1]
+print(f'The value in index 1 of list "this_is_a_list" is: {result1}')
+
+# Adding elements
+this_is_a_list.append(76)
+print(f'After appending 76 to "this_is_a_list" is: {this_is_a_list}')
+
+# Modifiying an element
+this_is_a_list[2] = 'hello'
+print(f'After editing the third element (index == 2): {this_is_a_list}')
+
+# Removing the first element of the list
+this_is_a_list.pop(0)
+print(f'After deleting the first element: {this_is_a_list}')
