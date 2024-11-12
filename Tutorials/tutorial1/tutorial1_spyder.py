5c65bfa731b6722f1382259e880e2ca5fce814b8
Evert Ingwersen <everti2000@gmail.com>
2024-11-12 11:59:27 +0100

    import libraries

--- Tutorials/tutorial1/tutorial1_spyder.py
+++ Tutorials/tutorial1/tutorial1_spyder.py
@@ -5,4 +5,14 @@ Created on Tue Nov 12 11:53:38 2024
 @author: evert
 """
 
-change hier 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import biogeme.database as db
+import biogeme.biogeme as bio
+from biogeme import models
+from biogeme.expressions import Beta, Variable, log, exp
+
+
+test_numpy = np.zeros(3)
