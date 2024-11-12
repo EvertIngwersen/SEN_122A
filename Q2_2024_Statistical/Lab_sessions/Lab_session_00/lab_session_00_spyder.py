a37e1656975d4448ca81fd140e9e7c201f1772a8
Evert Ingwersen <everti2000@gmail.com>
2024-11-12 12:47:41 +0100

    added dividend

---
+++ Q2_2024_Statistical/Lab_sessions/Lab_session_00/lab_session_00_spyder.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 12:24:44 2024
+
+@author: evert
+"""
+
+dividend = 7
+divisor = 3
+
+quotient = dividend / divisor
+int_quotient = int(quotient)
+
+remainder = dividend % divisor 
+
