056eeece59f72653b5e757039ade5e113726cf4e
Evert Ingwersen <everti2000@gmail.com>
2024-11-18 15:45:43 +0100

    added lybrary

--- Q2_2024_Statistical/Lab_sessions/Lab_session_01/lab_session_01_draft.py
+++ Q2_2024_Statistical/Lab_sessions/Lab_session_01/lab_session_01_draft.py
@@ -5,3 +5,17 @@ Created on Mon Nov 18 15:44:11 2024
 @author: evert
 """
 
+# Biogeme
+import biogeme.database as db
+import biogeme.biogeme as bio
+from biogeme import models
+from biogeme.expressions import Beta, Variable, log, exp
+
+# General python packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+from pathlib import Path
+
+# Pandas setting to show all columns when displaying a pandas dataframe
+pd.set_option('display.max_columns', None)
