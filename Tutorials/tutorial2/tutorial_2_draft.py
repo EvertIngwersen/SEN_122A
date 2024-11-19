a75b661fc91620fb96a3afff3073024ba1fabfae
Evert Ingwersen <everti2000@gmail.com>
2024-11-19 12:11:16 +0100

    last commit before VIX

--- Tutorials/tutorial2/tutorial_2_draft.py
+++ Tutorials/tutorial2/tutorial_2_draft.py
@@ -14,3 +14,15 @@ import biogeme.database as db
 import biogeme.biogeme as bio
 from biogeme import models
 from biogeme.expressions import Beta, Variable, log
+import colorful as cf
+
+data_path =  Path(f'data/synthetic_VTTdata_tutorial1.dat')
+df = pd.read_csv(data_path)
+df.head(10)
+
+β_tt = -0.2
+β_tc = -0.5
+
+print(cf.green("Set of β's:"))
+print(cf.yellow(f"β_tt = {β_tt}"))
+print(cf.yellow(f"β_tc = {β_tc}"))
