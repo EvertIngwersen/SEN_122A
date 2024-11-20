649fb4e7f6fd19d265eebca46bee3fc8c3af4a02
Evert Ingwersen <everti2000@gmail.com>
2024-11-20 16:18:51 +0100

    added avg function

--- Tutorials/tutorial2/call_options_header.py
+++ Tutorials/tutorial2/call_options_header.py
@@ -24,20 +24,57 @@ g = Call('GOOG', d=3, m=11, y=2023, strike=200)
 
 stock_list = ['AAPL', 'XOM', 'MA', 'NVO', 'SAP']
 
+ΔP_dict = {}
+keys = range(len(stock_list))
+
+for i in keys:
+    ΔP_dict[i] = stock_list[i]
+
+print(ΔP_dict)
+
+
+
+ΔP_matrix = np.zeros((5,1))
+
+for i in range(len(stock_list)):
+    ΔP_matrix[i,0] = i
+
+change_list_AAPL = []
+change_list_XOM = []
+change_list_MA = []
+change_list_NVO = []
+change_list_SAP = []
 
 while True:
     for i in range(len(stock_list)):
+        print("-------------------")
         s = Stock(stock_list[i])
         price = s.price
         change = s.change
+        ΔP_matrix[i,0] = change
         last_trade = s.last_trade
-        print("-------------------")
+        if i == 0:
+            change_list_AAPL.append(change)
+            print(cf.blue("AVG ΔP =", sum(change_list_AAPL)/len(change_list_AAPL)))
+        if i == 1:
+            change_list_XOM.append(change)
+            print(cf.blue("AVG ΔP =", sum(change_list_XOM)/len(change_list_XOM)))
+        if i == 2:
+            change_list_MA.append(change)
+            print(cf.blue("AVG ΔP =", sum(change_list_MA)/len(change_list_MA)))
+        if i == 3:
+            change_list_NVO.append(change)
+            print(cf.blue("AVG ΔP =", sum(change_list_NVO)/len(change_list_NVO)))
+        if i == 4:
+            change_list_SAP.append(change)
+            print(cf.blue("AVG ΔP =", sum(change_list_SAP)/len(change_list_SAP)))
         print(cf.yellow(s))
         if change < 0:     
             print(cf.red((f" PRICE: {price}$")))
             print(cf.red(" ΔP < 0"))
             print(cf.red(f" CHANGE: {change}$"))
             print(cf.red("UTILITY LEVEL = LOW"))
+            print()
         else:
             print(cf.green((f" PRICE: {price}$")))
             print(cf.green(" ΔP > 0"))
