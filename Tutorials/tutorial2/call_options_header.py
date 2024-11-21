406a9bfa6615812d6345a066c6496ab8979fbfd1
Evert Ingwersen <everti2000@gmail.com>
2024-11-21 14:32:02 +0100

    added swicht case

--- Tutorials/tutorial2/call_options_header.py
+++ Tutorials/tutorial2/call_options_header.py
@@ -11,7 +11,9 @@ from datetime import datetime
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 
+switch = "average"
 start_time = datetime.now()
 timeout = time.time() + 200
 
@@ -45,6 +47,8 @@ change_list_MA = []
 change_list_NVO = []
 change_list_SAP = []
 
+
+
 while True:
     for i in range(len(stock_list)):
         print("-------------------")
@@ -54,26 +58,29 @@ while True:
         ΔP_matrix[i,0] = change
         last_trade = s.last_trade
         print(cf.yellow(s))
-        if i == 0:
-            change_list_AAPL.append(change)
-            average = sum(change_list_AAPL)/len(change_list_AAPL)
-            print(cf.yellow(f"AVG ΔP = {average}"))
-        if i == 1:
-            change_list_XOM.append(change)
-            average = sum(change_list_XOM)/len(change_list_XOM)
-            print(cf.yellow(f"AVG ΔP = {average}"))
-        if i == 2:
-            change_list_MA.append(change)
-            average = sum(change_list_MA)/len(change_list_MA)
-            print(cf.yellow(f"AVG ΔP = {average}"))
-        if i == 3:
-            change_list_NVO.append(change)
-            average = sum(change_list_NVO)/len(change_list_NVO)
-            print(cf.yellow(f"AVG ΔP = {average}"))
-        if i == 4:
-            change_list_SAP.append(change)
-            average = sum(change_list_SAP)/len(change_list_SAP)
-            print(cf.yellow(f"AVG ΔP = {average}"))
+        match switch:
+            case average:
+                if i == 0:
+                    change_list_AAPL.append(change)
+                    average = sum(change_list_AAPL)/len(change_list_AAPL)
+                    print(cf.yellow(f"AVG ΔP = {average}"))
+                if i == 1:
+                    change_list_XOM.append(change)
+                    average = sum(change_list_XOM)/len(change_list_XOM)
+                    print(cf.yellow(f"AVG ΔP = {average}"))
+                if i == 2:
+                    change_list_MA.append(change)
+                    average = sum(change_list_MA)/len(change_list_MA)
+                    print(cf.yellow(f"AVG ΔP = {average}"))
+                if i == 3:
+                    change_list_NVO.append(change)
+                    average = sum(change_list_NVO)/len(change_list_NVO)
+                    print(cf.yellow(f"AVG ΔP = {average}"))
+                if i == 4:
+                    change_list_SAP.append(change)
+                    average = sum(change_list_SAP)/len(change_list_SAP)
+                    print(cf.yellow(f"AVG ΔP = {average}"))
+            
         if change < 0:     
             print(cf.red((f" PRICE: {price}$")))
             print(cf.red(" ΔP < 0"))
@@ -86,6 +93,7 @@ while True:
             print(cf.green(f" CHANGE: {change}$"))
             print(cf.green("UTILITY LEVEL = HIGH"))
         print("LAST TRADE", last_trade)
+        
     if time.time() > timeout:
         print(cf.blue("MARKETS CLOSED"))
         break
